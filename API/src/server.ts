0450d7c4f38cc55122bd314b4bc977f2cf58375f
Allan Julie <allanzinhofontes@gmail.com>
2021-03-03 16:06:43 -0300

    api finished

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,15 +1,4 @@
-import 'reflect-metadata';
-import express from "express";
+import { app } from "./app";
 
-const app = express();
-
-import "./database";
-
-
-app.get("/", (requst, response) => {
-  response.json({message: "hello world"});
-});
 
 app.listen(5555, () => console.log("Server is running!"));
-
-export { app };
